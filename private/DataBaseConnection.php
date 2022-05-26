4fe12fbb6808930c32a4aecb1ad6260ecb8e43c1
Jorge <jorlopeztohme@gmail.com>
2022-05-25 23:43:50 -0300

    Added database connection class

---
+++ private/DataBaseConnection.php
@@ -0,0 +1,42 @@
+<?php 
+
+  class DB {
+
+    private $host;
+    private $database;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct()
+    {
+      $this -> host      = 'localhost';
+      $this -> database  = 'movielog';
+      $this -> user      = 'root';
+      $this -> password  = "";
+      $this -> charset   = 'utf8mb4';
+    }
+
+    public function connect() 
+    {
+      try 
+      {
+
+        $connection = "mysql:host" . $this->host . ";dbname=" . $this->database . ";charset=" . $this->charset;
+        $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES => false];
+        $pdo = new PDO($connection, $this->user, $this->password, $options);
+
+      }
+      catch(PDOException $e)
+      {
+
+        print_r("Error connection: " . $e->getMessage());
+
+      }
+
+    }
+    
+  }
+
+?>
