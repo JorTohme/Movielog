0fa20a833d407eb272d4427d1849c25b89c96059
Jorge <jorlopeztohme@gmail.com>
2022-05-28 17:34:24 -0300

    Minor changes, implementing user sessions

--- index.php
+++ index.php
@@ -2,76 +2,86 @@
 <html lang="es">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./public/assets/logo.svg" rel="icon">
-  
-  <link href="./public/css/style.css" rel="stylesheet">
-  <link href="./public/css/style2.css" rel="stylesheet">
-  <link href="./public/css/bootstrap-grid.min.css" rel="stylesheet">
-  <title>Movielog</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./public/assets/logo.svg" rel="icon">
+
+    <link href="./public/css/style.css" rel="stylesheet">
+    <link href="./public/css/style2.css" rel="stylesheet">
+    <link href="./public/css/bootstrap-grid.min.css" rel="stylesheet">
+    <title>Movielog</title>
+    <?php
+    session_start();
+    ?>
 </head>
 
 <body>
-  <header>
-    <nav>
-      <div id="logo">
-        <img src="./public/assets/logo.svg" alt="" id="logo">
-        <h1> Movielog </h1>
-      </div>
-      <div id="menu">
-        <a href="./sign-in.php">Iniciar Sesión</a>
-        <a>Crear Cuenta</a>
-        <a href="./index.php">Catálogo</a>
-        <a>Nosotros</a>
-        <input type="text" placeholder="Search">
-      </div>
-    </nav>
-  </header>
+    <header>
+        <nav>
+            <div id="logo">
+                <img src="./public/assets/logo.svg" alt="" id="logo">
+                <h1> Movielog </h1>
+            </div>
+            <div id="menu">
+                <?php 
+                if (isset($_SESSION['user'])) {
+                    $name = $_SESSION['user'];
+                    echo "<a> Hola $name </a>";
+                    echo "<a href='./private/sign-out.php'> Salir <a>";
+                } else {
+                    echo "<a href='./sign-in.php?sign-into-account'>Iniciar Sesión</a>";
+                    echo "<a href='./sign-in.php?register'>Crear Cuenta</a>";
+                }
+                ?>
+                <a href="./index.php">Catálogo</a>
+                <a>Nosotros</a>
+                <input type="text" placeholder="Search">
+            </div>
+        </nav>
+    </header>
 
-  <h2> Últimas películas </h2>
-  <div id="maincontent">
-    <div class="container movies-container">
-      <div class="row justify-content-around">
-        <div class="col-lg-3 movie">
-          <a href="">
-            <img class="movieposter" src="./public/assets/posters/spidermanposter.jpg" alt="">
-            <p class="movie-title">Spider-Man No Way Home</p>
-            <p>Catálogo</p>
-          </a>
+    <h2> Últimas películas </h2>
+    <div id="maincontent">
+        <div class="container movies-container">
+            <div class="row justify-content-around">
+                <div class="col-lg-3 movie">
+                    <a href="">
+                        <img class="movieposter" src="./public/assets/posters/spidermanposter.jpg" alt="">
+                        <p class="movie-title">Spider-Man No Way Home</p>
+                        <p>Catálogo</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 movie">
+                    <a href="">
+                        <img class="movieposter" src="./public/assets/posters/batmanposter.jpg" alt="">
+                        <p class="movie-title">The Batman (2022)</p>
+                        <p>Catálogo</p>
+                    </a>
+                </div>
+                <div class="col-lg-3 movie">
+                    <a href="">
+                        <img class="movieposter" src="./public/assets/posters/fantasticbeastaposter.jpg" alt="">
+                        <p class="movie-title">Fantastic Beasts</p>
+                        <p>Catálogo</p>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-3 movie">
-          <a href="">
-            <img class="movieposter" src="./public/assets/posters/batmanposter.jpg" alt="">
-            <p class="movie-title">The Batman (2022)</p>
-            <p>Catálogo</p>
-          </a>
-        </div>
-        <div class="col-lg-3 movie">
-          <a href="">
-            <img class="movieposter" src="./public/assets/posters/fantasticbeastaposter.jpg" alt="">
-            <p class="movie-title">Fantastic Beasts</p>
-            <p>Catálogo</p>
-          </a>
-        </div>
-
-      </div>
     </div>
-  </div>
-  <script src="./public/js/bootstrap.min.js"></script>
+    <script src="./public/js/bootstrap.min.js"></script>
 </body>
 
 <footer>
-  <div class="info">
-    <p id="name">By <span>Jorge L. Tohmé</span></p>
-    <p> <a href="https://github.com/JorTohme">Github</a> </p>
-  </div>
+    <div class="info">
+        <p id="name">By <span>Jorge L. Tohmé</span></p>
+        <p> <a href="https://github.com/JorTohme">Github</a> </p>
+    </div>
 </footer>
 
 <div class="shopping-cart">
-  <img src="./public/assets/shoppingcart.svg" alt="">
-  <p>(0)</p>
+    <img src="./public/assets/shoppingcart.svg" alt="">
+    <p>(0)</p>
 </div>
 
 </html>
--- sign-in.php
+++ sign-in.php
@@ -1,51 +1,28 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="./public/css/bootstrap.min.css" rel="stylesheet">
-  <link href="./public/assets/logo.svg" rel="icon">
-  <link href="./public/css/signin.css" rel="stylesheet">
-  <style>
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      user-select: none;
+<?php
+    if(isset($_GET['sign-into-account'])) {
+        include_once('./public/sign-into-account.php');
+    } else if (isset($_GET['register'])) {
+        include_once('./public/register.php');
+    } else {
+        echo "
+        <!DOCTYPE html>
+        <html lang='es'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <link rel='stylesheet' href='./public/css/404.css'>
+            <title>404</title>
+        </head>
+        <body>
+            <div class='error-message'>
+                <h1>Oops, este lugar no existe ¿?</h1>
+                <p>ERROR: 404</p>
+                <p>Página no encontrada</p>
+            </div>
+        </body>
+        </html>
+";
+            
     }
-
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-      }
-    }
-  </style>
-  <title>Signin</title>
-</head>
-
-<body class="text-center">
-  <main class="form-signin">
-    <form action="" method="POST">
-      <a href="./index.php"><img class="mb-4" src="./public/assets/logo.svg" alt="" width="122,4" height="96,9"></a>
-      <h1 class="h3 mb-3">Inicio de sesión</h1>
-      <div class="form-floating">
-        <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Direccion de e-mail</label>
-      </div>
-      <div class="form-floating">
-        <input type="password" class="form-control" name="password" placeholder="Password">
-        <label for="floatingPassword">Contraseña</label>
-      </div>
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Recuerdame
-        </label>
-      </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Ingresar</button>
-    </form>
-  </main>
-</body>
-
-</html>
+?>
