e652984f5c3bfa9d6d37e893d83d70af13e3aa9f
Jorge <jorlopeztohme@gmail.com>
2022-05-31 16:42:48 -0300

    Added new shopping cart page

---
+++ shoppingcart.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/shoppingcart.css">
+    <title>Carrito de compras</title>
+</head>
+
+<body>
+    <?php
+    include_once('./public/header.php')
+    ?>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+</body>
+
+</html>
