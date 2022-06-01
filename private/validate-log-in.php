b9a26adaeb70eb6eba944f8f417e3e76cb666ba1
Jorge <jorlopeztohme@gmail.com>
2022-06-01 12:31:58 -0300

    Added admin session

--- private/validate-log-in.php
+++ private/validate-log-in.php
@@ -7,12 +7,19 @@ $password = $_POST['password'];
 $request = "SELECT * FROM users WHERE email='$email' AND pass='$password'";
 $response = mysqli_query($db, $request);
 
-$requestUserName = "SELECT username FROM users WHERE email='$email' AND pass='$password'";
 
 if (mysqli_num_rows($response)) {
     mysqli_close($db);
     session_start();
-    $_SESSION['user'] = mysqli_fetch_assoc($response)['username'];
+
+
+    if (mysqli_fetch_assoc($response)['admin'] == 1)
+    {
+        $_SESSION['admin'] = true;
+    } else {
+        $_SESSION['user'] = mysqli_fetch_assoc($response)['username'];
+    }
+
     header("location:../index.php");
 } else {
     mysqli_close($db);
