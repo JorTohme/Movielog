aa1fe34a88befd057fed348ef9ecbca6b6ba8851
Jorge <jorlopeztohme@gmail.com>
2022-06-01 12:42:43 -0300

    Testing page for the Shopping Cart

--- shoppingcart.php
+++ shoppingcart.php
@@ -6,18 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/css/shoppingcart.css">
+    <?php session_start(); ?>
     <title>Carrito de compras</title>
 </head>
 
 <body>
-    <?php
-    include_once('./public/header.php')
-    ?>
+    <?php include_once('./public/header.php') ?>
     <main>
         <div class="container">
-
+            
         </div>
     </main>
 </body>
-
+<footer>
+    <div class="info">
+        <p id="name">By <span>Jorge L. Tohmé</span></p>
+        <p> <a href="https://github.com/JorTohme">Github</a> </p>
+    </div>
+</footer>
 </html>
