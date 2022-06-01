30a88b716d4cb4ec3760fe7cf1b99565b11456de
Jorge <jorlopeztohme@gmail.com>
2022-06-01 12:42:14 -0300

    Added movies view from the database

--- index.php
+++ index.php
@@ -6,50 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./public/assets/logo.svg" rel="icon">
-
     <link href="./public/css/style.css" rel="stylesheet">
     <link href="./public/css/style2.css" rel="stylesheet">
     <link href="./public/css/bootstrap-grid.min.css" rel="stylesheet">
     <title>Movielog</title>
-    <?php
-    session_start();
-    ?>
+    <?php include('./private/movies.php'); session_start(); ?>
 </head>
 
 <body>
-    <?php
-    include_once('./public/header.php')
-    ?>
-
+    <?php include_once('./public/header.php'); ?>
     <h2> Últimas películas </h2>
     <div id="maincontent">
         <div class="container movies-container">
             <div class="row justify-content-around">
-                <div class="col-lg-3 movie">
-                    <a href="">
-                        <img class="movieposter" src="./public/assets/posters/spidermanposter.jpg" alt="">
-                        <p class="movie-title">Spider-Man No Way Home</p>
-                        <p>Catálogo</p>
-                    </a>
-                </div>
-                <div class="col-lg-3 movie">
-                    <a href="">
-                        <img class="movieposter" src="./public/assets/posters/batmanposter.jpg" alt="">
-                        <p class="movie-title">The Batman (2022)</p>
-                        <p>Catálogo</p>
-                    </a>
-                </div>
-                <div class="col-lg-3 movie">
-                    <a href="">
-                        <img class="movieposter" src="./public/assets/posters/fantasticbeastaposter.jpg" alt="">
-                        <p class="movie-title">Fantastic Beasts</p>
-                        <p>Catálogo</p>
-                    </a>
-                </div>
+            <?php
+                while ($row = mysqli_fetch_row($result)) {
+                   echo "
+                        <div class='col-lg-3 movie'>
+                            <a href='./merch.php?movie=$row[0]'>
+                                <img class='movieposter' src='./public/assets/posters/$row[1]' alt=''>
+                                <p class='movie-title'>$row[0]</p>
+                                <p>Catálogo</p>
+                            </a>
+                        </div> "; 
+                }
+            ?>
             </div>
         </div>
     </div>
-    <script src="./public/js/bootstrap.min.js"></script>
 </body>
 
 <footer>
