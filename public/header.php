ea9e7f958a59ee8ec986a38eef4a8a398435fdc5
Jorge <jorlopeztohme@gmail.com>
2022-06-01 12:39:32 -0300

    Added admin session

--- public/header.php
+++ public/header.php
@@ -1,12 +1,15 @@
 <header>
-        <nav>
-            <div id="logo">
-                <img src="./public/assets/logo.svg" alt="" id="logo">
-                <h1> Movielog </h1>
-            </div>
-            <div id="menu">
-                <?php 
-                if (isset($_SESSION['user'])) {
+    <nav>
+        <div id="logo">
+            <img src="./public/assets/logo.svg" alt="" id="logo">
+            <h1> Movielog </h1>
+        </div>
+        <div id="menu">
+            <?php 
+                if (isset($_SESSION['admin'])) {
+                    echo "<a href='./admin.php'>Espacio del administrador</a>";
+                    echo "<a href='./private/sign-out.php'> Salir <a>";
+                } else if (isset($_SESSION['user'])) {
                     $name = $_SESSION['user'];
                     echo "<a> Hola $name </a>";
                     echo "<a href='./private/sign-out.php'> Salir <a>";
@@ -15,9 +18,9 @@
                     echo "<a href='./sign-in.php?register'>Crear Cuenta</a>";
                 }
                 ?>
-                <a href="./index.php">Catálogo</a>
-                <a>Nosotros</a>
-                <input type="text" placeholder="Search">
-            </div>
-        </nav>
-    </header>
+            <a href="./index.php">Catálogo</a>
+            <a>Nosotros</a>
+            <input type="text" placeholder="Search">
+        </div>
+    </nav>
+</header>
