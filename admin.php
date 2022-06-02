8f23782add4187c20b75c6774e9393ea1c23d99c
Jorge <jorlopeztohme@gmail.com>
2022-06-02 13:27:14 -0300

    Added style for the admin page

--- admin.php
+++ admin.php
@@ -17,10 +17,48 @@
         die();
     }
     ?>
+    <link rel="stylesheet" href="./public/css/admin.css">
 </head>
 
 <body>
-    <h1>SUBIR ARCHIVOS</h1>
+    <?php include './public/header.php'?>
+    <h2>AÑADIR PELÍCULA</h2>
+    <main>
+        <form action="" method="POST">
+            <h3 class="h3">Título</h3>
+            <div class="title-container">
+                <input name="movietitle" id="titleInput" required>
+            </div>
+            <div class="container">
+                <div class="container-element-1">
+                    <h3 class="h3">Subir poster</h3>
+                    <input type="file" name="poster" required>
+                </div>
+                <div class="container-element-2">
+                    <h3 class="h3">Productos</h3>
+                    <div class="product">
+                        <p>Foto del producto 1</p>
+                        <input type="file" name="merch1-picture" required>
+                        <p>Nombre del producto 1</p>
+                        <input name="merch1-name" width="20px" required>
+                    </div>
+                    <div class="product">
+                        <p>Foto del producto 2</p>
+                        <input type="file" name="merch2" required>
+                        <p>Nombre del producto 1</p>
+                        <input name="merch1-name" width="20px" required></td>
+                    </div>
+                    <div class="product">
+                        <p>Foto del producto 3</p>
+                        <input type="file" name="merch3" required>
+                        <p>Nombre del producto 1</p>
+                        <input name="merch1-name" width="20px" required></td>
+                    </div>
+                </div>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit" id="submit-button">SUBIR</button>
+        </form>
+    </main>
 </body>
 
 </html>
